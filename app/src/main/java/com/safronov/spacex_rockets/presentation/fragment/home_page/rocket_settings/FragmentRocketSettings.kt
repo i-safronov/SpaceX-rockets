e1369f701f40f2be7332ba684dd65d8d4b6de51c
1337safa <ilya.safa1337w@gmail.com>
2023-07-14 20:19:03 +0300

    Added the ability to customize the display in which unit of measurement to display information about the rocket

--- app/src/main/java/com/safronov/spacex_rockets/presentation/fragment/home_page/rocket_settings/FragmentRocketSettings.kt
+++ app/src/main/java/com/safronov/spacex_rockets/presentation/fragment/home_page/rocket_settings/FragmentRocketSettings.kt
@@ -9,7 +9,10 @@ import android.view.ViewGroup
 import android.widget.RadioButton
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
+import com.safronov.domain.model.UnitOfMeasurementRocketDiameter
 import com.safronov.domain.model.UnitOfMeasurementRocketHeight
+import com.safronov.domain.model.UnitOfMeasurementRocketMass
+import com.safronov.domain.model.UnitOfMeasurementRocketPayload
 import com.safronov.spacex_rockets.core.extension.logE
 import com.safronov.spacex_rockets.core.extension.toastS
 import com.safronov.spacex_rockets.databinding.FragmentRocketSettingsBinding
@@ -41,6 +44,9 @@ class FragmentRocketSettings : Fragment() {
         viewLifecycleOwner.lifecycleScope.launch {
             val initButtonsHeight = async {
                 initButtonsUnitOfMeasurementRocketHeight()
+                initButtonsUnitOfMeasurementRocketDiameter()
+                initButtonsUnitOfMeasurementRocketMass()
+                initButtonsUnitOfMeasurementRocketPayload()
             }
             initButtonsHeight.await()
         }
@@ -49,10 +55,46 @@ class FragmentRocketSettings : Fragment() {
     private fun initButtonsUnitOfMeasurementRocketHeight() {
         fragmentRocketSettingsViewModel.getTypeRocketHeightMeasurement {
             viewLifecycleOwner.lifecycleScope.launch(Dispatchers.Main) {
-                if (it == UnitOfMeasurementRocketHeight.M.toString()) {
+                if (it.toLowerCase() == UnitOfMeasurementRocketHeight.M.toString().toLowerCase()) {
                     _binding?.rButtonRocketHeightM?.isChecked = true
-                } else if (it == UnitOfMeasurementRocketHeight.FT.toString()) {
+                } else if (it.toLowerCase() == UnitOfMeasurementRocketHeight.FT.toString().toLowerCase()) {
                     _binding?.rButtonRocketHeightFt?.isChecked = true
+                }
+            }
+        }
+    }
+
+    private fun initButtonsUnitOfMeasurementRocketDiameter() {
+        fragmentRocketSettingsViewModel.getTypeRocketDiameterMeasurement {
+            viewLifecycleOwner.lifecycleScope.launch(Dispatchers.Main) {
+                if (it.toLowerCase() == UnitOfMeasurementRocketDiameter.FT.toString().toLowerCase()) {
+                    _binding?.rButtonRocketDiameterFt?.isChecked = true
+                } else if (it.toLowerCase() == UnitOfMeasurementRocketDiameter.M.toString().toLowerCase()) {
+                    _binding?.rButtonRocketDiameterM?.isChecked = true
+                }
+            }
+        }
+    }
+
+    private fun initButtonsUnitOfMeasurementRocketMass() {
+        fragmentRocketSettingsViewModel.getTypeRocketMassMeasurement {
+            viewLifecycleOwner.lifecycleScope.launch(Dispatchers.Main) {
+                if (it.toLowerCase() == UnitOfMeasurementRocketMass.KG.toString().toLowerCase()) {
+                    _binding?.rButtonRocketMassKg?.isChecked = true
+                } else if (it.toLowerCase() == UnitOfMeasurementRocketMass.LB.toString().toLowerCase()) {
+                    _binding?.rButtonRocketMassLb?.isChecked = true
+                }
+            }
+        }
+    }
+
+    private fun initButtonsUnitOfMeasurementRocketPayload() {
+        fragmentRocketSettingsViewModel.getTypeRocketPayloadMeasurement {
+            viewLifecycleOwner.lifecycleScope.launch(Dispatchers.Main) {
+                if (it.toLowerCase() == UnitOfMeasurementRocketPayload.KG.toString().toLowerCase()) {
+                    _binding?.rButtonRocketPayloadKg?.isChecked = true
+                } else if (it.toLowerCase() == UnitOfMeasurementRocketPayload.LB.toString().toLowerCase()) {
+                    _binding?.rButtonRocketPayloadLb?.isChecked = true
                 }
             }
         }
@@ -62,6 +104,9 @@ class FragmentRocketSettings : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         try {
             unitOfMeasurementRocketHeightRadioButtonsListener()
+            unitOfMeasurementRocketDiameterRadioButtonsListener()
+            unitOfMeasurementRocketMassRadioButtonsListener()
+            unitOfMeasurementRocketPayLoadRadioButtonsListener()
             tvCancelListener()
         } catch (e: Exception) {
             logE("${this.javaClass.name} -> ${object{}.javaClass.enclosingMethod?.name}")
@@ -75,6 +120,39 @@ class FragmentRocketSettings : Fragment() {
                 fragmentRocketSettingsViewModel.saveTypeRocketHeightMeasurement(UnitOfMeasurementRocketHeight.M)
             } else if (button.text.toString().trim().toLowerCase() == UnitOfMeasurementRocketHeight.FT.toString().toLowerCase()) {
                 fragmentRocketSettingsViewModel.saveTypeRocketHeightMeasurement(UnitOfMeasurementRocketHeight.FT)
+            }
+        }
+    }
+
+    private fun unitOfMeasurementRocketDiameterRadioButtonsListener() {
+        binding.radioGroup2.setOnCheckedChangeListener { radioGroup, id ->
+            val button = requireActivity().findViewById<RadioButton>(id)
+            if (button.text.toString().toLowerCase() == UnitOfMeasurementRocketDiameter.FT.toString().toLowerCase()) {
+                fragmentRocketSettingsViewModel.saveTypeRocketDiameterMeasurement(UnitOfMeasurementRocketDiameter.FT)
+            } else if (button.text.toString().toLowerCase() == UnitOfMeasurementRocketDiameter.M.toString().toLowerCase()) {
+                fragmentRocketSettingsViewModel.saveTypeRocketDiameterMeasurement(UnitOfMeasurementRocketDiameter.M)
+            }
+        }
+    }
+
+    private fun unitOfMeasurementRocketMassRadioButtonsListener() {
+        binding.radioGroup3.setOnCheckedChangeListener { radioGroup, id ->
+            val button = requireActivity().findViewById<RadioButton>(id)
+            if (button.text.toString().toLowerCase() == UnitOfMeasurementRocketMass.KG.toString().toLowerCase()) {
+                fragmentRocketSettingsViewModel.saveTypeRocketMassMeasurement(UnitOfMeasurementRocketMass.KG)
+            } else if (button.text.toString().toLowerCase() == UnitOfMeasurementRocketMass.LB.toString().toLowerCase()) {
+                fragmentRocketSettingsViewModel.saveTypeRocketMassMeasurement(UnitOfMeasurementRocketMass.LB)
+            }
+        }
+    }
+
+    private fun unitOfMeasurementRocketPayLoadRadioButtonsListener() {
+        binding.radioGroup4.setOnCheckedChangeListener { radioGroup, id ->
+            val button = requireActivity().findViewById<RadioButton>(id)
+            if (button.text.toString().toLowerCase() == UnitOfMeasurementRocketPayload.KG.toString().toLowerCase()) {
+                fragmentRocketSettingsViewModel.saveTypeRocketPayloadMeasurement(UnitOfMeasurementRocketPayload.KG)
+            } else if (button.text.toString().toLowerCase() == UnitOfMeasurementRocketPayload.LB.toString().toLowerCase()) {
+                fragmentRocketSettingsViewModel.saveTypeRocketPayloadMeasurement(UnitOfMeasurementRocketPayload.LB)
             }
         }
     }
--- app/src/main/java/com/safronov/spacex_rockets/presentation/fragment/home_page/rocket_settings/view_model/FragmentRocketSettingsViewModel.kt
+++ app/src/main/java/com/safronov/spacex_rockets/presentation/fragment/home_page/rocket_settings/view_model/FragmentRocketSettingsViewModel.kt
@@ -2,7 +2,10 @@ package com.safronov.spacex_rockets.presentation.fragment.home_page.rocket_setti
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.safronov.domain.model.UnitOfMeasurementRocketDiameter
 import com.safronov.domain.model.UnitOfMeasurementRocketHeight
+import com.safronov.domain.model.UnitOfMeasurementRocketMass
+import com.safronov.domain.model.UnitOfMeasurementRocketPayload
 import com.safronov.domain.repository.RocketSettingsRepositoryInt
 import com.safronov.spacex_rockets.core.extension.logE
 import kotlinx.coroutines.Dispatchers
@@ -22,10 +25,70 @@ class FragmentRocketSettingsViewModel(
         }
     }
 
+    fun getTypeRocketDiameterMeasurement(result: (String) -> Unit) {
+        try {
+            viewModelScope.launch(Dispatchers.IO) {
+                result.invoke(rocketSettingsRepositoryInt.getTypeRocketDiameterMeasurement())
+            }
+        } catch (e: Exception) {
+            logE("${this.javaClass.name} -> ${object {}.javaClass.enclosingMethod?.name}, ${e.message}")
+        }
+    }
+
+    fun getTypeRocketMassMeasurement(result: (String) -> Unit) {
+        try {
+            viewModelScope.launch(Dispatchers.IO) {
+                result.invoke(rocketSettingsRepositoryInt.getTypeRocketMassMeasurement())
+            }
+        } catch (e: Exception) {
+            logE("${this.javaClass.name} -> ${object {}.javaClass.enclosingMethod?.name}, ${e.message}")
+        }
+    }
+
+    fun getTypeRocketPayloadMeasurement(result: (String) -> Unit) {
+        try {
+            viewModelScope.launch(Dispatchers.IO) {
+                result.invoke(rocketSettingsRepositoryInt.getTypeRocketPayloadMeasurement())
+            }
+        } catch (e: Exception) {
+            logE("${this.javaClass.name} -> ${object {}.javaClass.enclosingMethod?.name}, ${e.message}")
+        }
+    }
+
     fun saveTypeRocketHeightMeasurement(unitOfMeasurement: UnitOfMeasurementRocketHeight) {
         try {
             viewModelScope.launch(Dispatchers.IO) {
                 rocketSettingsRepositoryInt.saveTypeRocketHeightMeasurement(unitOfMeasurement = unitOfMeasurement)
+            }
+        } catch (e: Exception) {
+            logE("${this.javaClass.name} -> ${object {}.javaClass.enclosingMethod?.name}, ${e.message}")
+        }
+    }
+
+    fun saveTypeRocketDiameterMeasurement(unitOfMeasurement: UnitOfMeasurementRocketDiameter) {
+        try {
+            viewModelScope.launch(Dispatchers.IO) {
+                rocketSettingsRepositoryInt.saveTypeRocketDiameterMeasurement(unitOfMeasurement = unitOfMeasurement)
+            }
+        } catch (e: Exception) {
+            logE("${this.javaClass.name} -> ${object {}.javaClass.enclosingMethod?.name}, ${e.message}")
+        }
+    }
+
+    fun saveTypeRocketMassMeasurement(unitOfMeasurement: UnitOfMeasurementRocketMass) {
+        try {
+            viewModelScope.launch(Dispatchers.IO) {
+                rocketSettingsRepositoryInt.saveTypeRocketMassMeasurement(unitOfMeasurement = unitOfMeasurement)
+            }
+        } catch (e: Exception) {
+            logE("${this.javaClass.name} -> ${object {}.javaClass.enclosingMethod?.name}, ${e.message}")
+        }
+    }
+
+    fun saveTypeRocketPayloadMeasurement(unitOfMeasurement: UnitOfMeasurementRocketPayload) {
+        try {
+            viewModelScope.launch(Dispatchers.IO) {
+                rocketSettingsRepositoryInt.saveTypeRocketPayloadMeasurement(unitOfMeasurement = unitOfMeasurement)
             }
         } catch (e: Exception) {
             logE("${this.javaClass.name} -> ${object {}.javaClass.enclosingMethod?.name}, ${e.message}")
